fa47fd7d25bc0b4ef01f7538f814fa0362796e3a
Miguel Manzano <mmanzano@gmail.com>
2015-04-08 11:24:03 +0200

    Seeders funcionando

--- app/database/seeds/CategoryTableSeeder.php
+++ app/database/seeds/CategoryTableSeeder.php
@@ -1,20 +1,28 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
 class CategoryTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
-		{
-			Category::create([
+        Category::create([
+            'id'   => 1,
+            'name' => 'Backend developers',
+            'slug' => 'backend-developers'
+        ]);
 
-			]);
-		}
+        Category::create([
+            'id'   => 2,
+            'name' => 'Frontend developers',
+            'slug' => 'frontend-developers'
+        ]);
+
+        Category::create([
+            'id'   => 3,
+            'name' => 'Designers',
+            'slug' => 'designers'
+        ]);
+
 	}
 
 }
