afd99a46577514f10ac73c4fc05ae46db9b3fb10
Miguel Manzano <mmanzano@gmail.com>
2015-04-24 15:00:43 +0200

    Implementacion de la home

--- app/HireMe/Repositories/CandidateRepo.php
+++ app/HireMe/Repositories/CandidateRepo.php
@@ -1,6 +1,7 @@
 <?php namespace HireMe\Repositories;
 
 use HireMe\Entities\Candidate;
+use HireMe\Entities\Category;
 
 class CandidateRepo extends BaseRepo {
 
@@ -8,4 +9,17 @@ class CandidateRepo extends BaseRepo {
     {
         return new Candidate;
     }
+
+    public function findLatest($take = 10)
+    {
+
+        return Category::with([
+            'candidates' => function ($q) use ($take){
+                $q->take($take);
+                $q->orderBy('created_at', 'DESC');
+			},
+            'candidates.user'
+        ])->get();
+    }
+
 }
